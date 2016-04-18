27a85ec9002cafd941a4e63a6581c1d5c35dcae3
gregfagan <gregfagan@gmail.com>
2016-04-18 13:22:36 -0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: 'build',
+    filename: 'index.js',
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: 'node_modules/html-webpack-template/index.ejs',
+      appMountId: 'app',
+      devServer: 'http://localhost:8080',
+      mobile: true,
+    })
+  ],
+};
